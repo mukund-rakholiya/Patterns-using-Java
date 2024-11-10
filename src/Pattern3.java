4723a035cbb6bc66cd79348995eb40e384a07445
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-10 10:20:43 +0530

    Update Pattern3.java

--- src/Pattern3.java
+++ src/Pattern3.java
@@ -1,11 +1,12 @@
 public class Pattern3 {
     public static void main(String[] args) {
         for (int i = 1; i < 6; i++) {
+            
             for (int j = 5; j > i; j--)
                 System.out.print("  ");
 
             for (int k = 1; k <= i; k++)
-                System.out.print("* ");
+                System.out.print("* "); 
 
             System.out.println();
         }
