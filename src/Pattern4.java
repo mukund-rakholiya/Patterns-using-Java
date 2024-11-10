891e51220d2e369d18308d4a89d34f4b84dac201
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-10 10:22:36 +0530

    Update Pattern4.java

--- src/Pattern4.java
+++ src/Pattern4.java
@@ -3,7 +3,7 @@ public class Pattern4 {
         for (int i = 1; i < 6; i++) {
             for (int j = 5; j > i; j--)
                 System.out.print("  ");
- 
+  
             for (int k = 1; k <=i * 2 - 1; k++)
                 System.out.print("* ");
 
