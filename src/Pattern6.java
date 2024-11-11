c8e763170de124fee074c3eb5c82b51db2e3c413
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-11 12:51:55 +0530

    Update Pattern6.java

--- src/Pattern6.java
+++ src/Pattern6.java
@@ -14,7 +14,7 @@ public class Pattern6 {
 
             System.out.println();
         }
-
+ 
         for (int i = 1; i <= n / 2; i++) {
             for (int j = 1; j <= i; j++)
                 System.out.print("  ");
