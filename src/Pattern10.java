be5ba9ef34aba1fe83c46a908132140d581d55b6
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-15 09:50:05 +0530

    Update Pattern10.java

--- src/Pattern10.java
+++ src/Pattern10.java
@@ -5,7 +5,7 @@ public class Pattern10 {
         for (int i = 0; i < n; i++) {
 
             for (int j = 0; j < n; j++) 
-            
+             
                 if (i == 0 || i == n - 1) 
                     System.out.print("* ");
                 else if (j == 0 || j == n - 1 || j == i || j == n - i - 1)
