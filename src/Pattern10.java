c5fa2b412b7c1d92d816a92847756be0a1132808
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-26 16:50:08 +0530

    Update Pattern10.java

--- src/Pattern10.java
+++ src/Pattern10.java
@@ -1,7 +1,7 @@
 public class Pattern10 {
     public static void main(String[] args) {
         int n = 9;
-
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) 
              
