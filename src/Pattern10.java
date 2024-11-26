612d1296c21d7f4a09188585a28915d2245664a1
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-26 16:49:51 +0530

    Update Pattern10.java

--- src/Pattern10.java
+++ src/Pattern10.java
@@ -3,7 +3,6 @@ public class Pattern10 {
         int n = 9;
 
         for (int i = 0; i < n; i++) {
-
             for (int j = 0; j < n; j++) 
              
                 if (i == 0 || i == n - 1) 
