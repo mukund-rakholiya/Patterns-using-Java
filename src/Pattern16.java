c82172aeb8ffaf60b9ee9d72575ce80fa17663f3
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-12-10 08:00:15 +0530

    Update Pattern16.java

--- src/Pattern16.java
+++ src/Pattern16.java
@@ -5,7 +5,7 @@ public class Pattern16 {
             for (int j = n; j > i; j--) System.out.print(" ");
             for (int k = 1; k <= i; k++) System.out.print("* ");
             System.out.println();
-        }
+         }
         for (int i = 2; i <= n; i++) {
             for (int j = i; j < n; j++) System.out.print(" ");
             for (int k = 1; k <= i; k++) System.out.print("* ");
