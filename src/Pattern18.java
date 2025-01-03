3c17307fb60e598506500ed70d7bd7b627f5d30c
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2025-01-03 14:59:05 +0530

    Update Pattern18.java

--- src/Pattern18.java
+++ src/Pattern18.java
@@ -1,6 +1,6 @@
 public class Pattern18 {
     public static void main(String[] args) { 
-        int n = 5;
+        int n = 5; 
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) System.out.print(" ");
             for (int k = 1; k <= 2 * i - 1; k++) {
