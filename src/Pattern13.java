fda6fc603be13db31f08a9b4d5cb1637a796ce06
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2025-01-04 09:16:26 +0530

    Update Pattern13.java

--- src/Pattern13.java
+++ src/Pattern13.java
@@ -1,6 +1,6 @@
 public class Pattern13 {
     public static void main(String[] args) { 
-        int n = 5;
+        int n = 5; 
         for (int i = n; i >= 1; i--) {
             for (int j = n; j > i; j--) System.out.print(" ");
             for (int k = 1; k <= i; k++) System.out.print("* ");
