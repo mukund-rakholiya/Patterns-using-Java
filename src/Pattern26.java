c38330cb62f9bc2db5181e51acf20fdd69d53213
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2025-01-12 09:13:15 +0530

    Update Pattern26.java

--- src/Pattern26.java
+++ src/Pattern26.java
@@ -1,5 +1,5 @@
 public class Pattern26 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int n = 5; // Size of the square
 
         for (int i = 1; i <= n; i++) {
